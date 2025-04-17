d68062e08c536198aa048a683e653cad33045283
Joshua Taranto <jtaranto@cienciayfe.edu.ec>
2025-04-17 15:50:50 -0500

    [ADD] Models

--- django_challenge/swapi/models.py
+++ django_challenge/swapi/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Terrain(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Climate(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Planet(models.Model):
+    name = models.CharField(max_length=100)
+    population = models.FloatField()
+    terrains = models.ManyToManyField(Terrain, related_name='planets')
+    climates = models.ManyToManyField(Climate, related_name='planets')
+
+    def __str__(self):
+        return self.name
